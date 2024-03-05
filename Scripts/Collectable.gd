ea0d23f3bbc3e76de84641330b7d63f4daf21887
Acostag1010 <66980614+Acostag1010@users.noreply.github.com>
2024-03-05 15:51:07 -0500

    Create Collectable.gd

---
+++ Scripts/Collectable.gd
@@ -0,0 +1,10 @@
+extends Area3D
+
+var is_collected = false
+
+func _on_body_entered(_body):
+	# when player enters
+	# increment coin count by 1
+	global.coin_count = global.coin_count +1
+	print("coin count", global.coin_count)
+	queue_free() # remove object from scene
