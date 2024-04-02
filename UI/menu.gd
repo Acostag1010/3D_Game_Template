cfd08978ba35a302f8da72d4b2e15ac25c3be435
Acostag1010 <66980614+Acostag1010@users.noreply.github.com>
2024-04-02 17:40:39 -0400

    worked on bugs

--- UI/menu.gd
+++ UI/menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
-@export var start_level_scene: PackedScene
+@export_file var start_level_scene
 
 func _on_start_pressed():
-	get_tree().change_scene_to_packed(start_level_scene)
+	get_tree().change_scene_to_file(start_level_scene)
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+
+func _on_coin_collectable_item_collected():
+	pass # Replace with function body.
